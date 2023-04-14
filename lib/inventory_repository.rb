5d9a91b1e3cae4f82d4991305e1c2c8ca41ae809
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-04-14 10:38:00 +0100

    all method implemented and tested

--- lib/inventory_repository.rb
+++ lib/inventory_repository.rb
@@ -3,6 +3,17 @@ require_relative 'database_connection'
 
 class InventoryRepository
     def all
-        
+        sql = 'SELECT * FROM inventory;'
+        result_set = DatabaseConnection.exec_params(sql,[])
+        inventory = []
+        result_set.each do |field|
+            stock = Inventory.new
+            stock.item = field['item']
+            stock.price = field['price']
+            stock.quantity = field['quantity']
+            inventory << stock
+        end
+
+        return inventory
     end
 end
